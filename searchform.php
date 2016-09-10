abe3778bdbfef694625c358f83fd1729ae7e2267
Cristina Robinson <cenamaria2787@gmail.com>
2016-09-10 09:47:58 -0700

    On focus placeholder text disappears
    Updated placeholder text to disappear on focus

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <form role="search" method="get" id="searchform" class="searchform" action="<?php echo home_url( '/' ); ?>">
     <div>
-        <input type="search" id="s" name="s" value="search" />
+        <input type="search" id="s" name="s" placeholder="search" />
 
         <button type="submit" id="searchsubmit" ><?php _e('Go','bonestheme'); ?></button>
     </div>
